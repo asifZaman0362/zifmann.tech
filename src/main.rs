57a87a4329810b3ce47b56bb25ab76331ae97fb5
Asif Zaman <zero362001@gmail.com>
2023-07-03 16:49:59 +0530

    added some files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files as fs;
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+/*
+#[get("/")]
+async fn index(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!".to_string())
+}
+*/
+
+#[actix::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            //.service(index)
+            .service(fs::Files::new("/", "./static/").index_file("index.html"))
+    })
+    .bind(("localhost", 8000))?
+    .run()
+    .await
 }
