9f24585448062d09be8dc583e219804a9f45d8bd
EC2 Default User <ec2-user@ip-172-31-4-90.ap-south-1.compute.internal>
2023-10-16 11:58:43 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_files as fs;
 use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use env_logger::Env;
 
 /*
 #[get("/")]
@@ -10,12 +12,15 @@ async fn index(_req: HttpRequest) -> impl Responder {
 
 #[actix::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(move || {
         App::new()
             //.service(index)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .service(fs::Files::new("/", "./static/").index_file("index.html"))
     })
-    .bind(("localhost", 8000))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
 }
